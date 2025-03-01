976a7497ccfc747f4ca1e52fcc216c0e6a058342
superkookai <weerawut@live.com>
2025-03-01 17:01:44 +0700

    Initial Commit

---
+++ GridPractice/GridPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridPracticeApp.swift
+//  GridPractice
+//
+//  Created by Weerawut Chaiyasomboon on 01/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct GridPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
